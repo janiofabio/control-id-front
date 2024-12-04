18b4cd67b881928c57453991b2bb5e3859d39430
janiofabio <janiofabio.queiroz@ggmail.com>
2024-12-04 17:45:37 -0300

    Remover arquivos TypeScript não utilizados

--- src/constants-go.ts
+++
@@ -1,5 +0,0 @@
-// constants.ts Go
-
-export const API_URL = "http://localhost:8080"; 
-export const TOKEN_KEY = "strapi-jwt-token";
-export const API_URL_AUTH = "http://localhost:8080/api"; 
--- src/pages/areas copy/index.ts
+++
@@ -1,4 +0,0 @@
-export { AreaCreate } from "./create";
-export { AreaEdit } from "./edit";
-export { AreaList } from "./list";
-export { AreaShow } from "./show";
